26d589f5dce1ad919a3eef6a74553708951ab33e
muhib <muhibmmk123@gmail.com>
2024-07-24 21:41:36 +0530

    fixed server.js to use mongodb atlas

--- CSI-BW/server.js
+++ CSI-BW/server.js
@@ -1,34 +1,40 @@
-require('dotenv').config()
-const express = require('express')
-const mongoose = require('mongoose')
-const path = require('path')
-const Article = require('./models/article')
-const articleRouter = require('./routes/articles')
-const methodOverride = require('method-override')
-const app = express()
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+const path = require('path');
+const Article = require('./models/article');
+const articleRouter = require('./routes/articles');
+const methodOverride = require('method-override');
+const app = express();
+
+console.log("MONGO_URI:", process.env.MONGO_URI);
 
 mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true
-})
-const PORT = 5000
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true
+});
 
-//setting view enjine as ejs 
-app.set('view engine', 'ejs')
-app.use(express.urlencoded({ extended: false }))
+const PORT = 5000;
+
+// Setting view engine as ejs
+app.set('view engine', 'ejs');
+app.use(express.urlencoded({ extended: false }));
 
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(methodOverride('_method'))
+app.use(methodOverride('_method'));
 
 app.get('/', async (req, res) => {
-  const articles = await Article.find().sort({ createdAt: 'desc' })
-  res.render('articles/index', { articles: articles })
-})
+  const articles = await Article.find().sort({ createdAt: 'desc' });
+  res.render('articles/index', { articles: articles });
+});
 
-app.use('/articles', articleRouter)
+app.use('/articles', articleRouter);
 
-
-//making sure connection is established with mongoDBCompass
+// Making sure connection is established with MongoDB Atlas
 mongoose.connection.once('open', () => {
-  console.log('Successfully connected to mongDB')
-  app.listen(PORT, () => { console.log(`Server running on port ${PORT}`) })
-})
+  console.log('Successfully connected to MongoDB Atlas');
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
+});
